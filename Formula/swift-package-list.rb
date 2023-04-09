a39484a5b2b19be4fe564015d3865ac71644e15d
FelixHerrmann <FelixHerrmann@users.noreply.github.com>
2023-04-09 12:29:21 +0000

    Bump swift-package-list to "$TAG"

--- Formula/swift-package-list.rb
+++ Formula/swift-package-list.rb
@@ -1,8 +1,8 @@
 class SwiftPackageList < Formula
   desc "A command-line tool to generate a JSON, PLIST, Settings.bundle or PDF file with all used SPM-dependencies of an Xcode project or workspace."
   homepage "https://github.com/FelixHerrmann/swift-package-list"
-  url "https://github.com/FelixHerrmann/swift-package-list/releases/download/2.1.3/swift-package-list.tar.gz"
-  sha256 "6ed9cdb4d84186c4c22c8387ae06cf748805b9718b57d5cb7955961595f683e4"
+  url "https://github.com/FelixHerrmann/swift-package-list/releases/download/1.0.6/swift-package-list.tar.gz"
+  sha256 "12bb3e82e72112a22f0d592a3da8d4ade6d3ce7bcea00cef057a4710605f7e94"
   license "MIT"
 
   def install
