a1bbab45448c4c6876c40574ed1014d63b1746df
FelixHerrmann <FelixHerrmann@users.noreply.github.com>
2023-09-21 20:39:29 +0000

    Bump swift-package-list to 3.0.9

--- Formula/swift-package-list.rb
+++ Formula/swift-package-list.rb
@@ -1,8 +1,8 @@
 class SwiftPackageList < Formula
   desc "A command-line tool to generate a JSON, PLIST, Settings.bundle or PDF file with all used SPM-dependencies of an Xcode project or workspace."
   homepage "https://github.com/FelixHerrmann/swift-package-list"
-  url "https://github.com/FelixHerrmann/swift-package-list/releases/download/3.0.8/swift-package-list.tar.gz"
-  sha256 "bd5860710f7de64eeaa5e564d0b57306ed2cb3bf15e27b20d71fb88ba60d4659"
+  url "https://github.com/FelixHerrmann/swift-package-list/releases/download/3.0.9/swift-package-list.tar.gz"
+  sha256 "2fda6e4aa7f6420bb94eb006006cc2daa0f3360c074f0d7316909df3e4e9cd13"
   license "MIT"
 
   def install
